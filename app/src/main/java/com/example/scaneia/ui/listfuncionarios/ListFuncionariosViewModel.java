47812cc35cdb583705c64965c850ddc24ff7be85
brendasoriano <158192315+brendasoriano@users.noreply.github.com>
2025-08-22 08:29:33 -0300

    adicionando activities no projeto mobile (#3)

---
+++ app/src/main/java/com/example/scaneia/ui/listfuncionarios/ListFuncionariosViewModel.java
@@ -0,0 +1,4 @@
+package com.example.scaneia.ui.listfuncionarios;
+
+public class ListFuncionariosViewModel {
+}
---
+++ app/src/main/java/com/example/scaneia/ui/perfil/ProfileViewModel.java
@@ -0,0 +1,4 @@
+package com.example.scaneia.ui.perfil;
+
+public class ProfileViewModel {
+}
