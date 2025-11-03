6ed60bbd6ebf1e9f7f40fe38c769b049ee2eb45b
MariaDawla <majudawla@hotmail.com>
2025-11-03 00:09:34 -0300

    feat: implementando a webView no powerBI

--- app/src/main/java/com/example/scaneia/analises/AnalisesFragment.java
+++ app/src/main/java/com/example/scaneia/analises/AnalisesFragment.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.ImageView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -20,6 +23,21 @@ public class AnalisesFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentAnalisesBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        WebView webView = root.findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true); // necessário para Power BI
+        webView.getSettings().setDomStorageEnabled(true); // ajuda no carregamento
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                view.loadUrl(request.getUrl().toString());
+                return true;
+            }
+        });
+
+        // URL do Power BI (embutida ou pública)
+        String powerBiUrl = "https://app.powerbi.com/view?r=eyJrIjoiOGMzOTM4YzYtMmRjMy00MzMxLTg4OTYtYmQ3ODg3NzBiNTBmIiwidCI6ImIxNDhmMTRjLTIzOTctNDAyYy1hYjZhLTFiNDcxMTE3N2FjMCJ9";
+        webView.loadUrl(powerBiUrl);
 
         ImageView profile = root.findViewById(R.id.profile);
         profile.setOnClickListener(new View.OnClickListener() {
